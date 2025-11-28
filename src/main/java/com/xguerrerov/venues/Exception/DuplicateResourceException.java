73c1ca7e4f25710f56181cdaa738510d9efb82e8
xguerrerov0903 <xguerrerov0903@gmail.com>
2025-11-28 06:41:14 -0500

    adios :D

--- src/main/java/com/xguerrerov/venues/Exception/DuplicateResourceException.java
+++
@@ -1,7 +0,0 @@
-package com.xguerrerov.venues.Exception;
-
-public class DuplicateResourceException extends RuntimeException {
-    public DuplicateResourceException(String message) {
-        super(message);
-    }
-}
